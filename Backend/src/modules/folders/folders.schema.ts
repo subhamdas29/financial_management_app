78f272dd41ce8cd36a53e4303466051ad3b8879a
subhamdas29 <subhamdas5477@gmail.com>
2026-05-14 04:14:10 +0530

    Folders schema added

---
+++ Backend/src/modules/folders/folders.schema.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+import { FolderType } from "@prisma/client";
+
+export const createFolderSchema = z.object({
+    body: z.object({
+        name: z.string().min(1,"Folder name is required"),
+        type: z.nativeEnum(FolderType),
+        color: z.string().optional(), //colour code
+        icon: z.string().optional(), //emoji
+        description: z.string().optional(),
+        budgetLimit: z.number().positive().optional()
+    }),
+});
+
+export const updateFolderSchema = z.object({
+    body: z.object({
+        name: z.string().min(1).optional(),
+        color: z.string().optional(),
+        icon: z.string().optional(),
+        description: z.string().optional(),
+        budgetLimit: z.number().positive().optional()
+    }),
+    params: z.object({
+        id: z.string().min(1)
+    }),
+});
+
+export type CreateFolderSchema = z.infer<typeof createFolderSchema>["body"];
+export type UpdateFolderSchema = z.infer<typeof updateFolderSchema>["body"];
